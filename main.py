328e3dccbd3ba053fd11a6986f1674c46073ad8a
Anubhav <anubhavchambyal@gmail.com>
2026-02-13 16:15:36 +0530

    B :q ` exit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 df=pd.read_csv(r'C:\Users\anubh\OneDrive\Desktop\test\insurance - insurance.csv')
 
-from sklearn.model_selection import train_test_split
 
 x=df.drop(columns=['charges'])
 y=df['charges']
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=22)
 
-from sklearn.preprocessing import OrdinalEncoder
 oe=OrdinalEncoder(categories=[['yes','no']])
-x_train_smoker=oe.fit_transforrm(x_train[['smoker']])
+x_train_smoker=oe.fit_transform(x_train[['smoker']])
 x_test_smoker=oe.transform(x_test[['smoker']])
 
 ohe=OneHotEncoder(drop='first',sparse_output=False)
@@ -28,8 +30,6 @@ x_test_f=np.concatenate([x_test_num,x_test_gender_region,x_test_smoker],axis=1)
 print(x_train_f.shape)
 print(x_test_f.shape) 
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 
 ct=ColumnTransformer([
     ('ohe',OneHotEncoder(sparse_output=False,drop='first'),['sex','region']),
