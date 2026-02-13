e251d37c6012a972fbe02e3e40cd104672e62c4e
Anubhav <anubhavchambyal@gmail.com>
2026-02-13 15:46:50 +0530

    second upload

--- main.py
+++ main.py
@@ -11,4 +11,17 @@ y=df['charges']
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=22)
 
 print(x_train.shape)
-print(x_test.shape)
+print(x_test.shape) 
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct=ColumnTransformer([
+    ('ohe',OneHotEncoder(sparse_output=False,drop='first'),['sex','smoker','region'])
+],remainder='passthrough')
+
+x_train_transformed=ct.fit_transform(x_train)
+x_test_transformed=ct.transform(x_test)
+
+print(x_train.shape)
+print(x_train_transformed.shape)
